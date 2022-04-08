c3694d20f3b4084d4ba1a437d79ff1ae3858a6b7
jk1834 <95237166+jk1834@users.noreply.github.com>
2022-04-07 22:04:11 -0500

    LOGIN AND SIGN UP FINALLY FKING WORK

--- door/TheDoor/views.py
+++ door/TheDoor/views.py
@@ -80,69 +80,55 @@ def is_special_character(password):
 
 # Handles all signup functionality
 def signup(request):
-
+    
     if request.method == "POST":
-
         # Records all information needed for checks and user creation
-        username = request.POST.get("username")
-        firstname = request.POST.get("firstname")
-        lastname = request.POST.get("lastname")
+        username = request.POST.get('username')
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
         email = None
-        pass1 = request.POST.get("pass1")
-        pass2 = request.POST.get("pass2")
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+
+        # Checks if username has already been taken
+        if User.objects.filter(username = username).first():
+            messages.error(request, "This username is already taken.")
+            return render(request, 'TheDoor/signup.html')
 
         # VALIDATION GOES HERE
-        # Checks if username has already been taken
-        if User.objects.filter(username=username).first():
-            messages.error(request, "This username is already taken.")
-            return render(request, "base.html")
-
         # Makes sure that all characters in username are only alphanumeric
         if not username.isalnum():
-            messages.error(
-                request, "Make sure your username only contains numbers and letters."
-            )
-            return render(request, "base.html")
-
+            messages.error(request, "Make sure your username only contains numbers and letters.")
+            return render(request, 'TheDoor/signup.html')
+        
         # Makes sure the password falls under specified character conditions
         if (not is_special_character(pass1)) or len(pass1) <= 8:
-            messages.error(
-                request,
-                'Your password must include at least one of the following: "! @ # $ % ^ & * - _ . /" You must also include at least 8 characters.',
-            )
-            return render(request, "base.html")
+            messages.error(request, "Your password must include at least one of the following: \"! @ # $ % ^ & * - _ . /\" You must also include at least 8 characters.")
+            return render(request, 'TheDoor/signup.html')
 
         # Makes sure the user's name only has alphabetical letters
         if (not firstname.isalpha()) or (not lastname.isalpha()):
-            messages.error(
-                request, "Your first and last name must only include the letters a-z."
-            )
-            return render(request, "base.html")
-
-        # Makes sure passwords match
-        if not (pass1 == pass2):
-            messages.error(request, "Your passwords must match.")
-            return render(request, "TheDoor/signup")
+            messages.error(request, "Your first and last name must only include the letters a-z.")
+            return render(request, 'TheDoor/signup.html')
+    
 
         # Makes a new user and assigns basic properties, then saves that information to the Django database
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = firstname
         myuser.last_name = lastname
-
+        Profile(myuser).save()
         myuser.save()
 
         # Outputs success message and redirects user to the sign in page
         messages.success(request, "Your account has been successfully created!")
 
-        return render(request, "TheDoor/signin.html")
+        return render(request, 'TheDoor/signin.html')
+    return render(request, 'TheDoor/signup.html')
 
-    # messages.error(request, "There has been an error with the Signup page.")
-    # return render(request, "base.html")
-
-
+# Handles login functionality
 def signin(request):
 
-    if request.method == "GET":
+    if request.method == "POST":
 
         # Retrieves information from database
         username = request.POST.get("username")
@@ -154,13 +140,11 @@ def signin(request):
         # If the user exists, log them in and show them to the main page
         if user is not None:
             login(request, user)
-            fname = user.first_name
-            return render(request, "TheDoor/dashboard.html", {"fname": fname})
+            return render(request, "TheDoor/dashboard.html")
 
         # Else, return them to the home page with an error
         else:
             messages.error(request, "Incorrect Username or Password")
             return render(request, "TheDoor/signin.html")
 
-    # messages.error(request, "There has been an error with the Login Page.")
-    # return render(request, 'base.html')
+    return render(request, "TheDoor/signin.html")
--- door/door/settings.py
+++ door/door/settings.py
@@ -125,6 +125,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "dashboard"
-
-DEBUG = False
-ALLOWED_HOSTS = ["127.0.0.1"]
