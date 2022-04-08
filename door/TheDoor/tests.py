d5cee355feba63bbac9a3d9fae8ae464c1884201
jk1834 <95237166+jk1834@users.noreply.github.com>
2022-04-08 10:38:28 -0500

    Added some test cases

--- door/TheDoor/tests.py
+++ door/TheDoor/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+class UrlTests(TestCase):
+    def test_homePage(self):
+        response = self.client.get("/")
+        self.assertEquals(response.status_code, 200)
+    
+   # def test_homeURL(self):
+    #    response = self.client.get(reverse("dashboard"))
+     #   self.assertEquals(response.status_code, 200)
